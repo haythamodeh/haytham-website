c159f12af758744c49998f57e668a01f768d382c
HAYTHAM-PC\Haytham <haythamlion@outlook.com>
2018-01-23 01:14:39 -0800

    changed alot of stuff

--- validation.php
+++ validation.php
@@ -1,81 +1,226 @@
+<?php
+/*
+if(isset($_POST['email'])) {
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "haythamlion@outlook.com";
+    $email_subject = "Your email subject line";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $message = $_POST['message']; // required
+ 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(strlen($message) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Comments: ".clean_string($message)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+<!-- include your own success html here -->
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
+ 
+}*/
 
-<?php // Initialize variables to null.
-$name =""; // Sender Name
-$email =""; // Sender's email ID
-$purpose =""; // Subject of mail
-$message =""; // Sender's Message
-$nameError ="";
-$emailError ="";
-$purposeError ="";
-$messageError ="";
-$successMessage =""; // On submittingform below function will execute.
-if(isset($_POST['submit'])) { // Checking null values in message.
-if (empty($_POST["name"])){
-$nameError = "Name is required";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "haythamlion@outlook.com";
+
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$feedback_page = "index.html";
+$error_page = "error_message.html";
+$thankyou_page = "thank_you.html";
+
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$email_address = $_REQUEST['email'] ;
+$comments = $_REQUEST['message'] ;
+$first_name = $_REQUEST['name'] ;
+$msg = 
+"Name: " . $first_name . "\r\n" . 
+"Email: " . $email_address . "\r\n" . 
+"Message: " . $comments ;
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
-else
- {
-$name = test_input($_POST["name"]); // check name only contains letters and whitespace
-if (!preg_match("/^[a-zA-Z ]*$/",$name))
-{
-$nameError = "Only letters and white space allowed";
+
+// If the user tries to access this script directly, redirect them to the feedback form,
+if (!isset($_REQUEST['email'])) {
+header( "Location: $feedback_page" );
 }
-} // Checking null values inthe message.
-if (empty($_POST["email"]))
-{
-$emailError = "Email is required";
+
+// If the form fields are empty, redirect to the error page.
+elseif (empty($first_name) || empty($email_address) || empty($comments)) {
+    header( "Location: $error_page" );
 }
-else
- {
-$email = test_input($_POST["email"]);
-} // Checking null values inmessage.
-if (empty($_POST["purpose"]))
-{
-$purposeError = "Purpose is required";
+
+/* 
+If email injection is detected, redirect to the error page.
+If you add a form field, you should add it here.
+*/
+elseif ( isInjected($email_address) || isInjected($first_name)  || isInjected($comments) ) {
+    header( "Location: $error_page" );
 }
-else
-{
-$purpose = test_input($_POST["purpose"]);
-} // Checking null values inmessage.
-if (empty($_POST["message"]))
-{
-$messageError = "Message is required";
+
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+
+	mail( "$webmaster_email", "Contact Form", $msg, 'from website' );
+
+	header( "Location: $thankyou_page" );
 }
-else
- {
-$message = test_input($_POST["message"]);
-} // Checking null values inthe message.
-if( !($name=='') && !($email=='') && !($purpose=='') &&!($message=='') )
-{ // Checking valid email.
-if (preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
-{
-$header= $name."<". $email .">";
-$headers = "FormGet.com"; /* Let's prepare the message for the e-mail */
-$msg = "Hello! $name Thank you...! For Contacting Us.
-Name: $name
-E-mail: $email
-Purpose: $purpose
-Message: $message
-This is a Contact Confirmation mail. We Will contact You as soon as possible.";
-$msg1 = " $name Contacted Us. Hereis some information about $name.
-Name: $name
-E-mail: $email
-Purpose: $purpose
-Message: $message "; /* Send the message using mail() function */
-if(mail($email, $headers, $msg ) && mail("haythamlion@outlook.com", $header, $msg1 ))
-{
-$successMessage = "Message sent successfully.......";
-}
-}
-else
-{ $emailError = "Invalid Email";
- }
- }
-} // Function for filtering input values.function test_input($data)
-{
-$data = trim($data);
-$data =stripslashes($data);
-$data =htmlspecialchars($data);
-return $data;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
